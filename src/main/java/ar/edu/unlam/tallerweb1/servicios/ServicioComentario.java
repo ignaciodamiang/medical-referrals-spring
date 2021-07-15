3547a562554891a7f9689fd7172bb56c5bb204b9
MatiasGuerrero61 <guerreromatias24@gmail.com>
2021-07-14 23:44:10 -0300

    fix

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComentario.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComentario.java
@@ -5,10 +5,8 @@ import ar.edu.unlam.tallerweb1.modelo.Derivacion;
 import ar.edu.unlam.tallerweb1.modelo.SolicitudDerivacion;
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
 
-import javax.servlet.http.HttpServletRequest;
-
 public interface ServicioComentario {
     void guardarComentario(Comentario comentario);
-    void guardarComentarioDerivacion(Derivacion derivacion, String mensaje , Usuario usuario, String funcion) throws Exception;
+    void guardarComentarioDerivacion(Derivacion derivacion, String mensaje , Usuario usuario, String funcion);
     void guardarComentarioSolicitudDerivacion(SolicitudDerivacion solicitudDerivacion, String mensaje, Usuario usuario, String funcion);
 }
